ff7453c88a6002dccec2545d1f0204ab6a9058f1
Muhammadaminjon <muhammadaminovmuhadamin303@gamil.com>
2023-05-26 19:28:29 +0500

    hero section and about section and some response

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 AOS.init();
-const drop = document.querySelector(".dropdown");
-const lang = document.querySelector(".lang");
-const text = document.querySelector(".lang-text");
-drop.addEventListener("click", () => {
-  text.classList.toggle("active");
+// const drop = document.querySelector(".dropdown");
+// const lang = document.querySelector(".lang");
+// const text = document.querySelector(".lang-text");
+// drop.addEventListener("click", () => {
+//   text.classList.toggle("active");
+// });
+
+// text.addEventListener("click", () => {
+//   lang.classList.remove("active");
+// });
+
+const heroBtn = document.getElementById("hero-btn");
+const grey = document.querySelector(".grey");
+const heromodal = document.querySelector(".hero-modal");
+
+heroBtn.addEventListener("click", () => {
+  grey.style.display = "block";
+  heromodal.classList.toggle("active");
 });
 
-text.addEventListener("click", () => {
-  lang.classList.remove("active");
+grey.addEventListener("click", () => {
+  grey.style.display = "none";
+  heromodal.classList.remove("active");
 });

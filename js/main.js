6b54a263d1066c60cbf87a748c9e391a94b3d2bb
Muhammadaminjon <muhammadaminovmuhadamin303@gamil.com>
2023-06-07 11:15:55 +0500

    response all site

--- js/main.js
+++ js/main.js
@@ -42,12 +42,10 @@ grey.addEventListener("click", () => {
   heromodal.classList.remove("active");
 });
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   loop: true,
-  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
   },
   slidesPerView: "auto",
   spaceBetween: 20,
